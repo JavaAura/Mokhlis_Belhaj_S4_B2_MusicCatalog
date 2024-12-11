54f7072f8c89e464ab72de671a4219ba26dff688
BelhajMokhlis <belhajmokhlis@gmail.com>
2024-12-11 21:00:41 +0100

    first commit

---
+++ src/main/java/com/music/MusicCatalog/MokhlisBelhajS4B2MusicCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.music.MusicCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokhlisBelhajS4B2MusicCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokhlisBelhajS4B2MusicCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/music/MusicCatalog/MokhlisBelhajS4B2MusicCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.MusicCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MokhlisBelhajS4B2MusicCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
