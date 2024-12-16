7da50c54784b29c35c93aa0e2ab230bb96f687c2
BelhajMokhlis <belhajmokhlis@gmail.com>
2024-12-16 15:49:33 +0100

    add admine

--- src/main/java/com/music/MusicCatalog/config/defaultDataConfig.java
+++ src/main/java/com/music/MusicCatalog/config/defaultDataConfig.java
@@ -2,6 +2,7 @@ package com.music.MusicCatalog.config;
 
 import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -16,6 +17,15 @@ import com.music.MusicCatalog.Service.UserService;
 
 @Configuration
 public class defaultDataConfig {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
 
     @Order(1)
     @Bean
@@ -44,14 +54,14 @@ public class defaultDataConfig {
         userRequest.setUsername("admin");
         userRequest.setPassword("admin123");
         userService.registerUser(userRequest);
+        assignRoleToAdmin(userRepository, userService, roleRepository);
         return true;
         }
         return false;
     }
 
-    @Order(3)
-    @Bean
-    public boolean   defaultAdminRole(UserRepository userRepository, RoleRepository roleRepository, UserService userService) {
+    
+    public boolean   assignRoleToAdmin(UserRepository userRepository, UserService userService, RoleRepository roleRepository) {
          Users user = userRepository.findByUsername("admin").get();
         Role role = roleRepository.findByName("ROLE_ADMIN").get();
         userService.assignRoleToUser(user.getId(), role.getId());
